83a68b8b88084d66a2751a8994a7ff2f3a47afbe
Ahmad <eeeedayat@gmail.com>
2025-08-28 21:15:53 +0700

    Add files via upload

---
+++ video/php-smk/lat01.php
@@ -0,0 +1,20 @@
+<?php
+
+    echo "Belajar php";
+
+    echo '<br>';
+
+    echo 'Saya siswa smk';
+
+    echo '<h1 style="background-color:red">Belajar Php Coyyy</h1>
+         <p style="background-color:blue">Saya Laki-laki</p>'
+    ;
+
+    echo 'Saya belajar'.'php'.'<br>';
+
+    echo 'Tahun 2025'.'<br>';
+
+    echo 2023 + 2;
+
+?>
+
---
+++ video/php-smk/lat02.php
@@ -0,0 +1,7 @@
+<?php 
+
+    $tulisan = 'Saya belajar';
+
+    echo $tulisan;
+
+?>
