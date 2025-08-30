00275047d35dca916cf52624735a52deb98be623
Amad-Indoasat <eeeedayat@gmail.com>
2025-08-30 21:15:38 +0700

    pehape

---
+++ video/php-smk/lat05.php
@@ -0,0 +1,41 @@
+<?php 
+
+    $tanggal = 7;
+
+    //$hasil = $tanggal < 32;
+
+    /*if ($tanggal < 32) {
+        if ($tanggal > 0) {
+            echo 'Benar';
+        }else {
+            echo 'Salah';
+        }
+    }else {
+        echo 'Salah';
+    }*/
+
+    $nilai = 999;
+
+    // if ($nilai <= 100){
+    //     if ($nilai >=0) {
+    //         echo 'Nilai Benar';
+    //     }else {
+    //         echo 'NGUWAWOR';
+    //     }
+    // }else {
+    //     echo 'NGUWAWOR';
+    // }
+
+    // if ($nilai >= 0 && $nilai <= 100) {
+    //     echo 'nilai benar';
+    // }else {
+    //     echo 'NGUWAWOR';
+    // }
+
+    if ($nilai >=100 || $nilai <=0) {
+        echo 'NGUWAWOR';
+    }else {
+        echo 'GOKILLL';
+    }
+
+?>
---
+++ video/php-smk/switch.php
@@ -0,0 +1,51 @@
+<?php 
+
+    // $hari = 5;
+
+    // switch ($hari) {
+    //     case 1:
+    //         echo 'minggu';
+    //         break;
+    //     case 2:
+    //         echo 'senin';
+    //         break;
+    //     case 3:
+    //         echo 'selasa';
+    //         break;
+    //     case 4:
+    //         echo 'rabu';
+    //         break;
+    //     case 5:
+    //         echo 'kamis';
+    //         break;
+    //     case 6:
+    //         echo 'jumat';
+    //         break;
+    //     case 7:
+    //         echo 'sabtu';
+    //         break;
+        
+    //     default:
+    //         echo 'Hari mu nguwawor';
+    //         break;
+    // }
+
+    $pilihan = 'tambah';
+
+    switch ($pilihan) {
+        case 'tambah':
+            echo 'anda memilih tambah';
+            break;
+        case 'ubah':
+            echo 'anda memilih ubah';
+            break;
+        case 'hapus':
+            echo 'anda memilih hapus';
+            break;
+        
+        default:
+            echo 'pilihanmu gak ada';
+            break;
+    }
+
+?>
