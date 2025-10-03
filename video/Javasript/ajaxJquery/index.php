62c10a82d960910f2fd287d5eac593cf6f7720e8
Amad-Indoasat <eeeedayat@gmail.com>
2025-10-03 21:41:50 +0700

    js

--- video/Javasript/ajaxJquery/index.php
+++ video/Javasript/ajaxJquery/index.php
@@ -8,6 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="js/jquery.js"></script>
+    <script src="js/app.js" defer></script>
 </head>
 
 <body>
@@ -22,21 +23,25 @@
                 </div>
 
                 <div class="row">
+                    <div id="msg">
+                        
+                    </div>
                     <form>
                         <div class="mb-3">
+                            <input type="text" class="form-control" id="id" required aria-describedby="emailHelp">
                             <label for="exampleInputEmail1" class="form-label">Nama Pelanggan</label>
-                            <input type="text" class="form-control" id="pelanggan" aria-describedby="emailHelp">
+                            <input type="text" class="form-control" id="pelanggan" required aria-describedby="emailHelp">
                             <div id="emailHelp" class="form-text">Isi lah</div>
                         </div>
                         <div class="mb-3">
                             <label for="exampleInputPassword1" class="form-label">Alamat</label>
-                            <input type="text" class="form-control" id="alamat">
+                            <input type="text" class="form-control" id="alamat" required>
                         </div>
                         <div class="mb-3">
                             <label for="exampleInputPassword1" class="form-label">Telp</label>
-                            <input type="number" class="form-control" id="telp">
+                            <input type="number" class="form-control" id="telp" required>
                         </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <button type="submit" class="btn btn-primary">Simpan</button>
                     </form>
                 </div>
             </div>
@@ -54,19 +59,9 @@
                                 <th scope="col">Telp</th>
                             </tr>
                         </thead>
-                        <tbody>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                            </tr>
+                        <tbody id="isidata">
+
+
                         </tbody>
                     </table>
                 </div>
---
+++ video/Javasript/ajaxJquery/php/koneksi.php
@@ -0,0 +1,7 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$db = "dbrestoran";
+
+$conn = mysqli_connect($host,$user,$password,$db);
