015995333c487ec57f35f0d3a10043b9d11e9712
Amad-Indoasat <eeeedayat@gmail.com>
2025-10-16 09:20:19 +0700

    kelas

---
+++ Kelas/16-OKTOBER-2025/pages/prestasi.php
@@ -0,0 +1,6 @@
+<h1>Prestasi</h1>
+
+<p>Prea</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, tempore?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, tempore?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, tempore?</p>
---
+++ Kelas/16-OKTOBER-2025/pages/sejarah.php
@@ -0,0 +1,4 @@
+<h1>Sejarah</h1>
+
+<p>Selamat datang di SMKN 2 Buduran Sidoarjo</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, tempore?</p>
